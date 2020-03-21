fa17951aed360804d09701e822610d683d50640e
lgulich <lgulich@ethz.ch>
2020-03-21 09:09:35 +0100

    vundle -->  vim plug

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y git vim tmux zsh
 
 # Check that zsh is used.
 if [[ ! "$SHELL" =~ .*zsh.* ]]; then
-  read -p "Default shell is not zsh, do you want to change? (Y/n)"
+  read -rp "Default shell is not zsh, do you want to change? (Y/n)"
   echo
   if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
     chsh -s "$(which zsh)"
@@ -19,11 +19,15 @@ fi
 git -C "$SCRIPT_DIR" submodule init
 git -C "$SCRIPT_DIR" submodule update
 
+# Install vim-plug.
+curl -fLo "$SCRIPT_DIR"/vim/vim_plug/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Create the dotfiles for the home folder.
-printf "source $SCRIPT_DIR/zsh/zshrc_manager.sh" > ~/.zshrc
-printf "source $SCRIPT_DIR/vim/vimrc" > ~/.vimrc
-printf "source-file $SCRIPT_DIR/tmux/tmux.conf" > ~/.tmux.conf
-printf "source ~/.vimrc" > ~/.ideavimrc
+echo "source $SCRIPT_DIR/zsh/zshrc_manager.sh" > ~/.zshrc
+echo "source $SCRIPT_DIR/vim/vimrc" > ~/.vimrc
+echo "source-file $SCRIPT_DIR/tmux/tmux.conf" > ~/.tmux.conf
+echo "source ~/.vimrc" > ~/.ideavimrc
 
 ln -fs "$SCRIPT_DIR"/i3/config ~/.config/i3/config
 
