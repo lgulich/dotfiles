6573cbaecbf6db483ecab213d2a7bb6a1a005bd5
lgulich <lgulich@ethz.ch>
2020-03-22 10:31:50 +0100

    add vim headerguard

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,28 +1,29 @@
 SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
+DOTFILES="$(dirname "$SCRIPT_DIR")"
 
 # Run tmux if exists.
 if command -v tmux>/dev/null; then
 	if [ "$DISABLE_TMUX" = "true" ]; then
 		echo "DISABLE_TMUX=true"
 	else
-		[ -z $TMUX ] && exec tmux
+		[ -z "$TMUX" ] && exec tmux
 	fi
-else 
+else
 	echo "tmux not installed, could not autostart tmux"
 fi
 
 
 echo "Checking for updates..."
-git -C "$SCRIPT_DIR" fetch -q &> /dev/null 
+git -C "$DOTFILES" fetch -q &> /dev/null
 
-if [ $(git -C "$SCRIPT_DIR" rev-list HEAD...origin/master | wc -l) = 0 ]
+if [ "$(git -C "$DOTFILES" rev-list HEAD...origin/master | wc -l)" = 0 ]
 then
 	echo "Already up to date."
 else
 	echo "Updates Detected:"
-	({cd ~/dotfiles} &> /dev/null && git log ..@{u} --pretty=format:%Cred%aN:%Creset\ %s\ %Cgreen%cd)
+	git -C "$DOTFILES" log ..@{u} --pretty=format:%Cred%aN:%Creset\ %s\ %Cgreen%cd
 	echo "Setting up..."
-	({cd ~/dotfiles} &> /dev/null && git pull -q && git submodule update --init --recursive)
+	git -C "$DOTFILES" pull -q && git -C "$DOTFILES" submodule update --init --recursive
 fi
 
 source "$SCRIPT_DIR"/zshrc.sh
