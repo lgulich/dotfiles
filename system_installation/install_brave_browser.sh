6b824f52e8845338cc9519bef7d2839d9baa494a
lgulich <lgulich@ethz.ch>
2020-07-17 10:18:31 +0200

    Add brave browser beta version

--- system_installation/install_brave_browser.sh
+++ system_installation/install_brave_browser.sh
@@ -2,12 +2,11 @@
 
 sudo apt-get install -y apt-transport-https curl
 
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo \
-apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc \
+  | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" \
+  | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable \
-main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-sudo apt update
+sudo apt-get update
 sudo apt-get install -y brave-browser
---
+++ system_installation/install_brave_browser_beta.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -ex
+
+sudo apt-get install -y apt-transport-https curl
+
+curl -s https://brave-browser-apt-beta.s3.brave.com/brave-core-nightly.asc \
+  | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-prerelease.gpg add -
+
+echo "deb [arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ stable main" \
+  | sudo tee /etc/apt/sources.list.d/brave-browser-beta.list
+
+sudo apt-get update
+sudo apt-get install -y brave-browser-beta
