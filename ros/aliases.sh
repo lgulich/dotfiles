877e8805b4717b4f7761f9db8709aa11a7394b5b
lgulich <lgulich@ethz.ch>
2020-10-29 15:52:45 +0100

    update aliases

--- ros/aliases.sh
+++ ros/aliases.sh
@@ -1,5 +1,5 @@
 # shellcheck shell=sh
 
-alias ac='cd ~/arc_core'
-alias aws='cd ~/arc_ws'
+alias ac='~/arc_core'
+alias aws='~/arc_ws'
 alias kg="killall -9 gazebo & killall -9 gzserver  & killall -9 gzclient"
