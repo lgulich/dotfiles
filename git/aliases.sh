310c0f9d369c5619e0d7b3d30d0d6ac9cec89ff5
lg <l@s.com>
2022-08-02 20:45:30 +0200

    Pull new changes from downstream

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,12 +1,15 @@
 # shellcheck shell=sh
 
 # Git aliases
-alias nah='git reset --hard; git clean -df;'
+alias nah='git reset --hard && git clean -df;'
 alias gch='git checkout'
 alias gs='git status'
 alias ga='git add'
 alias gd='git diff --color'
 alias gc='git commit'
+alias gl='git log'
+alias grc='git rebase --continue'
+alias grm='git fetch && git rebase origin/master'
 alias gca='git commit --amend'
 alias gpr='git pull --rebase'
 alias gpublish='git_publish_branch'
