42e714f344e7d7a80f988a1bdfe8068b8a77a4e2
lg <l@s.com>
2022-11-26 16:22:33 +0100

    Move git count lines alias to its own script

---
+++ git/git_count_lines.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+author="${1:?}"
+
+git log --author="$author" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf \"added lines: %s, removed lines: %s, total lines: %s\\n\", add, subs, loc }'
