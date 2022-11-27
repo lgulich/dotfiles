72da0562a6083139551ca558e4ab587fa6946535
lg <l@s.com>
2022-11-27 17:00:35 +0100

    Add one password helpers

--- git/helpers.sh
+++ git/helpers.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=sh
 
-git_clean_up_branches(){
+git_clean_up_branches() {
   git fetch -p
   for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}')
   do
---
+++ one_password/helpers.sh
@@ -0,0 +1,5 @@
+# shellcheck shell=sh
+
+ngc_api_key() {
+  op item get e6vditmgvmvq6v7iugz2qw7hyq --fields label="NGC API KEY"
+}
