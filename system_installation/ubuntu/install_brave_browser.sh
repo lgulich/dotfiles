5dce5ac874a7ed7075986ea599a6ccf247b304e7
lgulich <lionel.gulich@gmail.com>
2023-02-23 09:37:58 +0100

    Install brave from snap

--- system_installation/ubuntu/install_brave_browser.sh
+++ system_installation/ubuntu/install_brave_browser.sh
@@ -2,13 +2,4 @@
 
 set -e
 
-sudo apt-get install -y apt-transport-https curl
-
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc \
-  | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" \
-  | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-sudo apt-get update
-sudo apt-get install -y brave-browser
+sudo snap install brave
