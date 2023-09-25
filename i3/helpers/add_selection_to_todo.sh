c51fdca7dff5dc2d2cdba0fe74f348048965bee5
lgulich <lionel.gulich@gmail.com>
2023-09-25 11:27:23 +0200

    Add zenity selection to todo helper

--- i3/helpers/add_selection_to_todo.sh
+++ i3/helpers/add_selection_to_todo.sh
@@ -5,6 +5,7 @@ set -e
 TODO_FILE=~/tasks.todo
 
 clipboard_content=`xclip -o`
+zenity --question --width 300 --text "Add\n'$clipboard_content'\nto TODO list?"
 echo "- [] $clipboard_content" >> $TODO_FILE
 notify-send "Added '$clipboard_content' to TODO list."
 
