97180cfe00984cc7e4ab6664995a6a7373dd7bb8
lgulich <lionel.gulich@gmail.com>
2023-10-04 09:48:56 +0200

    Add mode to bazel visualize script

--- bazel/bazel_visualize_rdeps.sh
+++ bazel/bazel_visualize_rdeps.sh
@@ -10,6 +10,7 @@ cd "${BUILD_WORKING_DIRECTORY:-.}"
 bazel=auto
 workspace='.'
 depth=2
+mode=png
 in='//...'
 
 # get command line arguments
@@ -37,6 +38,10 @@ while [ $# -gt 0 ]; do
       depth="$2"
       shift 2
       ;;
+    --mode)
+      mode="$2"
+      shift 2
+      ;;
     *)
       echo "Error: Invalid arguments: ${1} ${2}"
       exit 1
@@ -44,7 +49,7 @@ while [ $# -gt 0 ]; do
 done
 
 if [[ $bazel != 'bazel' && $bazel != 'dazel' && $bazel != 'auto' ]]; then
-  echo "Invalid bazel argument '${bazel}'. Valid choices: 'auto', 'bazel', 'dazel'."
+  echo "Error: Invalid 'bazel' argument '${bazel}'. Valid choices: 'auto', 'bazel', 'dazel'."
   exit 1
 fi
 
@@ -56,10 +61,26 @@ if [[ $bazel == auto ]]; then
   fi
 fi
 
+if [[ $mode == png ]]; then
+  output=graph
+elif [[ $mode == txt ]]; then
+  output=label
+else
+  echo "Error: Invalid 'mode' argument '${mode}. Valid choices: 'png', 'txt'."
+  exit 1
+fi
+
 cd $workspace
 
 echo "Querying the graph..."
-graph_description=$($bazel cquery "rdeps($in, $of, $depth)" --notool_deps --noimplicit_deps --keep_going --output=graph)
-echo "Visualizing the graph..."
-echo $graph_description | "dot" -Tsvg > rdeps.svg
-echo "To visualize the graph, open 'file:///$(pwd)/rdeps.svg' in your browser."
+graph_description=$($bazel cquery "rdeps($in, $of, $depth)" --notool_deps --noimplicit_deps --keep_going --output=$output)
+
+if [[ $mode == txt ]]; then
+  echo "Found reverse dependencies:"
+  echo "$graph_description"
+  exit 0
+elif [[ $mode == png ]]; then
+  echo "Visualizing the graph..."
+  echo $graph_description | "dot" -Tsvg > rdeps.svg
+  echo "To visualize the graph, open 'file:///$(pwd)/rdeps.svg' in your browser."
+fi
