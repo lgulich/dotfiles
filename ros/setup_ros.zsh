756b51c38ed45b217ecbc003dd215877bc559fcd
lgulich <lionel.gulich@gmail.com>
2023-10-27 11:56:23 +0200

    [ros] Add setup script

---
+++ ros/setup_ros.zsh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+function source_ros() {
+  [ -f /opt/ros/humble/setup.zsh ] && source /opt/ros/humble/setup.zsh
+  source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.zsh
+  source ~/workspaces/IsaacSim-ros_workspaces/humble_ws/install/setup.zsh
+}
+
+# For isaac sim:
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/lgulich/.local/share/ov/pkg/isaac_sim-2023.1.0/exts/omni.isaac.ros2_bridge/humble/lib
+#
+function setup_ros_isaac_sim() {
+  export ROS_DOMAIN_ID=0
+  export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+  export FASTRTPS_DEFAULT_PROFILES_FILE=~/workspaces/IsaacSim-ros_workspaces/fastdds_profile.xml
+}
+
+function setup_ros_marmot() {
+  local ip=$(getent hosts carter-v23-9.dyn.nvidia.com | awk '{ print $1 }')
+  export ROS_DOMAIN_ID=0
+  export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+  # export ROS_DISCOVERY_SERVER=$ip:11811
+  # export FASTRTPS_DEFAULT_PROFILES_FILE=/home/lgulich/fastdds_profile.xml
+}
+
+function setup_ros_local() {
+  # export ROS_DOMAIN_ID=0
+  # export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+}
+
+source_ros
+setup_ros_local
