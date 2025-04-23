63608a10156bbb014b5befc8f3e7424fdcf9547b
lgulich <lionel.gulich@gmail.com>
2025-04-24 00:14:30 +0200

    Update yabai

--- yabai/install_macos.sh
+++ yabai/install_macos.sh
@@ -4,4 +4,8 @@ set -e
 
 yabai --stop-service || true
 brew install koekeishiya/formulae/yabai
+
+# Allow running `sudo yabai --load-sa` without sudo password.
+echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
+
 yabai --start-service
