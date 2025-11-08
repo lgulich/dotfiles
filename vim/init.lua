e627ef03abd40f76dfe93e3e6dc1799dd46b78e5
Lionel Gulich <lgulich@nvidia.com>
2025-11-08 14:49:21 +0100

    Update vim

--- vim/init.lua
+++ vim/init.lua
@@ -4,15 +4,33 @@ vim.cmd('source $DOTFILES/vim/config/keymap.vim')
 vim.cmd('source $DOTFILES/vim/config/plug.vim')
 vim.cmd('source $DOTFILES/vim/config/plugins.vim')
 
--- LSP Setup (move to own file when becomes too big)
+-- LSP Setup (move to own file when becomes too big).
 -- ClangD
-require('lspconfig').clangd.setup{
-  filetypes = {'c', 'cpp', 'cxx', 'cc'}
+vim.lsp.config.clangd = {
+  cmd = { 'clangd' },
+  filetypes = { 'c', 'cpp', 'cxx', 'cc' },
+  root_markers = { '.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', 'compile_flags.txt', 'configure.ac', '.git' },
 }
 
-require('lspconfig').rust_analyzer.setup{}
-require('lspconfig').pyright.setup{}
-require('lspconfig').ts_ls.setup{}
+vim.lsp.config.rust_analyzer = {
+  cmd = { 'rust-analyzer' },
+  filetypes = { 'rust' },
+  root_markers = { 'Cargo.toml', 'rust-project.json' },
+}
+
+vim.lsp.config.pyright = {
+  cmd = { 'pyright-langserver', '--stdio' },
+  filetypes = { 'python' },
+  root_markers = { 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', 'Pipfile', 'pyrightconfig.json' },
+}
+
+vim.lsp.config.ts_ls = {
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx' },
+  root_markers = { 'package.json', 'tsconfig.json', 'jsconfig.json', '.git' },
+}
+
+vim.lsp.enable({ 'clangd', 'rust_analyzer', 'pyright', 'ts_ls' })
 
 require('remote-sshfs').setup({
   connections = {
